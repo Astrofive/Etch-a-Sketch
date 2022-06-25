6a5907cbfb8c7788432df35df5b682b8d361639b
Kevin <kevin.strebel@yahoo.com>
2022-06-24 22:21:28 -0700

    JS now adds divs correctly. Will now work on giving them dimension

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("hello world!");
+
+let width = 4;
+let height = 4;
+
+// For each height
+for(y = 1; y <= height; y++){
+    const row = document.createElement('div');
+    row.classList.add('row');
+    row.id = "row" + y;
+    document.querySelector('#grids').appendChild(row);
+
+    // Create a row of divs
+    for(x = 1; x <= width; x++){
+        const div = document.createElement('div');
+        div.classList.add('grid')
+        document.querySelector('#row' + y).appendChild(div);
+    }  
+}
