42e49f5f66da96a0f1954876b797f2df1a52684e
Kevin <kevin.strebel@yahoo.com>
2022-06-24 23:18:02 -0700

    Fixed bug, paintGrid() now adds event listener for drawing

--- script.js
+++ script.js
@@ -30,12 +30,13 @@ function paintGrid(){
             document.querySelector('#row' + y).appendChild(div);
         }  
     }
+
+    const squares = document.querySelectorAll('.grid');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            console.log("hover is working");
+            square.style.backgroundColor = "#333";
+        });
+    });
 }
 
-const squares = document.querySelectorAll('.grid');
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        console.log("hover is working");
-        square.style.backgroundColor = "#333";
-    });
-});
