338f44f9b455864eb19efa04e5a0ada6d5ccb9b0
Kevin <kevin.strebel@yahoo.com>
2022-06-26 15:40:46 -0700

    grid generates in certain fixed size regardless of count. Also added a single variable for width and height

--- script.js
+++ script.js
@@ -1,37 +1,47 @@
 console.log("hello world!");
 
 //default values
-let width = 4;
-let height = 4;
+let gridSize = 4;
 
 //button to enter custom dimensions
 const dimensionbutton = document.querySelector('.dimensionbutton');
 dimensionbutton.addEventListener('click', () => {
-    width = prompt('Enter width');
-    height = prompt('Enter height');
-    paintGrid();
+    gridSize = prompt('Enter grid size');
+    if (0 < gridSize && gridSize <= 100){
+        paintGrid();
+    }
+    else {
+        alert("value must be a number between 1 and 100");
+    }
+
+    
 });
 
 function paintGrid(){
-    //clear any preexisting grids
-    document.getElementById('grids').innerHTML = '';
+    // clear any preexisting grids
+    document.getElementById('container').innerHTML = '';
+
+    // create rows container
+    const rows = document.createElement('div');
+    rows.id = "rows";
+    document.querySelector('#container').appendChild(rows);
 
     // For each height
-    for(y = 1; y <= height; y++){
+    for(y = 1; y <= gridSize; y++){
         const row = document.createElement('div');
         row.classList.add('row');
         row.id = "row" + y;
-        document.querySelector('#grids').appendChild(row);
+        document.querySelector('#rows').appendChild(row);
 
         // Create a row of divs
-        for(x = 1; x <= width; x++){
+        for(x = 1; x <= gridSize; x++){
             const div = document.createElement('div');
-            div.classList.add('grid')
+            div.classList.add('square')
             document.querySelector('#row' + y).appendChild(div);
         }  
     }
 
-    const squares = document.querySelectorAll('.grid');
+    const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.addEventListener('mouseover', () => {
             console.log("hover is working");
